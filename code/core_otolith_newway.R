132534098b1ac60370dcb3cdfe03bc9fdc1c032f
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-03-09 19:22:00 -0800

    added constants, result objects, more comments, incorporated all years

--- code/core_otolith_newway.R
+++ code/core_otolith_newway.R
@@ -41,8 +41,22 @@ allocation <- table(read.csv("data/AIallocation420.csv")$STRATUM)
 ###############################################################################  
 collection <- readxl::read_xlsx("data/AI_core_collections.xlsx", 
                                 sheet = "2022 collection")
+collection <- subset(x = collection, subset = common_name != "walleye pollock")
 pollock_collection <- readxl::read_xlsx("data/AI_core_collections.xlsx", 
                                         sheet = "2022 pollock collection")
+
+##################################################
+####   Constants
+##################################################  
+years <- sort(unique(catch$CRUISE))
+n_years <- length(years)
+
+n_boot <- 420
+n_iters <- 1000
+
+species <- collection$common_name
+species_codes <- collection$species_code
+n_spp <- nrow(collection)
 
 ##################################################
 ####    Calculate total otolith collection across species and year  
@@ -51,7 +65,7 @@ pollock_collection <- readxl::read_xlsx("data/AI_core_collections.xlsx",
 ##################################################
 specimen <- subset(x = AI$specimen,
                    subset = SPECIMEN_SAMPLE_TYPE == 1 & 
-                     SPECIES_CODE %in% collection$species_code)
+                     SPECIES_CODE %in% species_codes)
 
 total_ages <- table(specimen$SPECIES_CODE, specimen$CRUISE)     
 
@@ -59,13 +73,14 @@ spp_age_names <- AI$species$COMMON_NAME[match(rownames(total_ages),
                                               AI$species$SPECIES_CODE)]
 rownames(total_ages) <- spp_age_names
 
-iyear = 201801
-
-## Subset the catches for iyear for just the interested species
+##################################################
+####   Widen the catch dataset and append management area to each haul
+##################################################  
+## Subset the catches for just the interested species
 sub_catch <- subset(x = AI$catch, 
-                    subset = SPECIES_CODE %in% collection$species_code & 
-                      CRUISE == iyear,
-                    select = c("HAULJOIN","SPECIES_CODE", "NUMBER_FISH"))
+                    subset = SPECIES_CODE %in% species_codes,
+                    select = c("HAULJOIN","SPECIES_CODE", 
+                               "NUMBER_FISH", "CRUISE"))
 
 ## Widen sub_catch to fill in zeros
 catch_wide <- tidyr::spread(data = sub_catch, 
@@ -79,7 +94,8 @@ catch_wide <- tidyr::spread(data = sub_catch,
 ## Remove hauls w/o lat/lon info.
 haul_locs <- subset(x = AI$haul, 
                     subset = HAULJOIN %in% catch_wide$HAULJOIN,
-                    select = c(HAULJOIN, START_LONGITUDE, START_LATITUDE, STRATUM))
+                    select = c(HAULJOIN, START_LONGITUDE, 
+                               START_LATITUDE, STRATUM))
 
 catch_wide[, c("LON", "LAT", "STRATUM")] <- 
   haul_locs[match(catch_wide$HAULJOIN, haul_locs$HAULJOIN), 
@@ -94,91 +110,124 @@ catch_wide$REG <- as.character(cut(x = catch_wide$LON_E,
                                    breaks = c(170, 177, 183, 190, 195), 
                                    labels = c("WAI", "CAI", "EAI", "SBS")))
 
-bootstrap_collections <- matrix(nrow = 100, ncol = 14)
+##################################################
+####   Empty Result Objects
+##################################################  
+boot_spp <- array(dim = c(n_iters, n_spp, n_years), 
+                  dimnames = list(NULL, species, years))
+boot_haul <- array(dim = c(n_iters, n_boot, n_years), 
+                   dimnames = list(NULL, NULL, years))
+boot_collect <- array(dim = c(n_iters, n_spp),
+                      dimnames = list(NULL, species))
 
-for (iter in 1:100) {
+##################################################
+####   Conduct bootstrap
+##################################################  
+for (iter in 1:n_iters) {
+  
   ## Set seed
   set.seed(iter * 23432)
   
-  ## Draw the bootstrap sample, stratified by stratum
-  boot_idx <- c()
+  ## Draw the bootstrap sample, stratified by stratum and year
+  bootstrap_hauls <- list()
   
-  for (istratum in sort(unique(catch_wide$STRATUM)) ) {
-    isample <- allocation[istratum]
-    boot_idx <- c(boot_idx, sample(x = which(catch_wide$STRATUM == istratum), 
-                                   size = isample, 
-                                   replace = TRUE))
+  for (iyear in years) {
+    boot_idx <- c()
+    for (istratum in sort(unique(catch_wide$STRATUM)) ) {
+      isample <- allocation[istratum]
+      boot_idx <- c(boot_idx, 
+                    sample(x = which(catch_wide$STRATUM == istratum
+                                     & catch_wide$CRUISE == iyear), 
+                           size = isample, 
+                           replace = TRUE))
+    }
+    bootstrap_hauls[[iyear]] <- catch_wide[boot_idx, ]
   }
   
-  bootstrap_hauls <- catch_wide[boot_idx, ]
-  
-  catch <- bootstrap_hauls[, paste(collection$species_code[1:14])]
-  # target <- collection$total_requested[1:14]
-  # threshold <- collection$criteria_value[1:14]
-
-  
-  fn_ <- function(collect, target, threshold, 
-                  return_what = c("obj", "total_by_spp")[1]) {
+  ## Optim function 
+  fn_ <- function(collect, 
+                  bootstrap_hauls_,
+                  collection_input, 
+                  return_what = c("obj", "total_by_spp", "total_by_haul")[1]) {
     
-    collection_by_haul <- matrix(data = 0,
-                                 nrow = nrow(catch), 
-                                 ncol = length(collect))
+    target <- collection_input$total_requested
+    threshold <- collection_input$criteria_value
+    spp_codes <- collection_input$species_code
+    names(collect) <- names(target) <- names(threshold) <- spp_codes
     
-    abs_diff <- 0
-    collection <- rep(0, length(collect))
+    collection_by_haul <- array(data = 0,
+                                dim = c(nrow(bootstrap_hauls_[[1]]), 
+                                        length(collect), 
+                                        length(bootstrap_hauls_)),
+                                dimnames = list(NULL, 
+                                                spp_codes, 
+                                                names(bootstrap_hauls_)))
     
-    for (ispp in 1:length(collect)){
-      collect_idx <- catch[, ispp] >= threshold[ispp]
-      
-      if(!all(collect_idx == F)) {
-        collection_by_haul[collect_idx, ispp] <- 
-          sapply(X = catch[collect_idx, ispp], 
-                 FUN = function(x) min(x, 
-                                       collect[ispp]) )
-        collection[ispp] <- sum(collection_by_haul[collect_idx, ispp])
+    for (ispp in paste0(spp_codes)) {
+      for (iyear in names(bootstrap_hauls_)) {
         
-        abs_diff <- abs_diff + abs(collection[ispp] - target[ispp])
+        collect_idx <- rep(TRUE, dim(collection_by_haul)[1])
+        if (ispp == "10262") {
+          region = bootstrap_hauls_[[iyear]]$REG 
+          collect_idx[region %in% c("WAI", "CAI")] <- FALSE
+        }
+        
+        collection_by_haul[collect_idx, ispp, iyear] <-
+          sapply(X = bootstrap_hauls_[[iyear]][collect_idx, ispp], 
+                 FUN = function(x) ifelse(test = x >= threshold[ispp], 
+                                          yes = min(x, collect[ispp]), 
+                                          no = 0))
       }
     }
     
+    collection_by_year <- apply(X = collection_by_haul, 
+                                MARGIN = 2:3, 
+                                FUN = sum)
+    
+    abs_diff <- sum(abs(sweep(x = collection_by_year, 
+                              MARGIN = 1, 
+                              STATS = target,
+                              FUN = "-")))
+    
     if(return_what == "obj") return(abs_diff)
-    if(return_what == "total_by_spp") return(collection_by_haul)
+    if(return_what == "total_by_spp") return(collection_by_year)
+    if(return_what == "total_by_haul") return(apply(X = collection_by_haul, 
+                                                    MARGIN = c(1, 3), 
+                                                    FUN = sum))
   }
   
-  threshold_ <- collection$criteria_value[1:14]
-  
-  test = optim(par = rep(1, 14), 
-               fn = fn_, method = "BFGS",
-               target = collection$total_requested[1:14],
-               threshold = threshold_,
+  ## Run Optimization
+  test = optim(par = rep(1, n_spp), 
+               fn = fn_, method = "BFGS", 
+               bootstrap_hauls_ = bootstrap_hauls,
+               collection_input = collection,
                return_what = "obj")
   
-  best_collect <- ceiling(test$par)
-  best_collect[best_collect > 15] <- 15
+  ## Save optimal results
+  boot_spp[iter, , ] <- fn_(collect = best_collect,
+                            bootstrap_hauls_ = bootstrap_hauls,
+                            collection_input = collection,
+                            return_what = "total_by_spp")
   
-  temp_collection <- fn_(collect = best_collect,
-                         target = collection$total_requested[1:14],
-                         threshold = threshold_, 
-                         return_what = "total_by_spp")
-  hist(rowSums(temp_collection))
-  sum(rowSums(temp_collection) > 30)
+  boot_haul[iter, , ] <- fn_(collect = best_collect,
+                             bootstrap_hauls_ = bootstrap_hauls,
+                             collection_input = collection,
+                             return_what = "total_by_haul")
   
-  colSums(temp_collection)
+  boot_collect[iter, ] <- test$par
   
-  bootstrap_collections[iter, ] <- test$par
-  
+  ## Print results
   print(paste0("Iteration: ", iter))
-  print(test$par)
+  print(cbind(data.frame(name = collection$common_name, 
+                         collect = round(test$par, 1)),
+              boot_spp[iter, ,]) )
+  
+  ## Periodially save results
+  if (iter%%10 == 0) save(list =  c("years", "n_years", "n_boot", "n_iters", 
+                                    "species", "species_codes", "n_spp",
+                                    "total_ages", 
+                                    "boot_spp", "boot_haul", "boot_collect"), 
+                          file = "results/bootstrap_results.RData")
+  
+  
 }
-
-for (ispp in 1:14) {
-  hist(bootstrap_collections[, ispp], freq = F, 
-       las = 1, 
-       xlim = c(0, 1.05 * max(bootstrap_collections[, ispp], 
-                       collection$SBS[ispp])),
-       main = collection$common_name[ispp], 
-       xlab = "Collection Rule (random per haul)")
-  abline(v = collection$SBS[ispp], lwd = 3, col = "red")
-}
-
-ceiling(apply(X = bootstrap_collections, MARGIN = 2, FUN = median))
